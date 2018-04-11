3cb8af620d4655d7a414c71fcb96cc5c971b82b9
BrianJZhang <brianfounder@hotmail.com>
2018-04-11 08:31:39 +0800

    initial commit

---
+++ src/main/java/cn/wheelsfactory/spring_boot_demo/HelloController.java
@@ -0,0 +1,13 @@
+package cn.wheelsfactory.spring_boot_demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello,this is a springboot demo";
+    }
+}
